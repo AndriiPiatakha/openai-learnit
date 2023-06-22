23ed7132d4ac52d490bbb6aa1983a9c4d4a53cb9
Andrey Pyatakha <andrey.pyatakha@gmail.com>
2023-06-23 01:08:05 +0300

    init commit

---
+++ src/main/java/com/itbulls/learnit/openai/OpenaiApplication.java
@@ -0,0 +1,13 @@
+package com.itbulls.learnit.openai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenaiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpenaiApplication.class, args);
+	}
+
+}
